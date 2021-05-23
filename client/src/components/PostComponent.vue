dcc41f8b737f180ba5d75428e183ed7d486a84d6
GuilhermeSantosCoelho <guilhermesantoscoelho2000@gmail.com>
2021-05-23 18:42:47 -0300

    Node server

---
+++ client/src/components/PostComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+const url = 'http://localhost:5000/api/posts'
+export default {
+  name: 'PostComponent',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ server/routes/api/posts.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const MongoDB = require('mongodb');
+const router = express.Router()
+const uri = "mongodb+srv://new_pass:mongodbweb@cluster0.vhdw6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoDB.MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+router.get('/', async (req, res) => {
+    const posts = await loadPostCollections();
+    res.send(await posts.find({}).toArray())
+})
+
+//Criar Post
+router.post('/', async (req, res) => {
+    const posts = await loadPostCollections();
+    console.log(req.body)
+
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    });
+    res.status(201).send();
+})
+
+//Criar Delete
+router.delete('/:id', async (req, res) => {
+    const posts = await loadPostCollections();
+    await posts.deleteOne({
+        _id: new MongoDB.ObjectID(req.params.id)
+    });
+    res.status(200).send();
+})
+
+async function loadPostCollections() {
+    await client.connect();
+    return client.db('view_express').collection('posts');
+}
+
+loadPostCollections();
+
+module.exports = router;
