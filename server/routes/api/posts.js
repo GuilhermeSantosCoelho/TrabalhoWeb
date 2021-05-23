1b2288780ce92e236b5eadfd3cdac1ef7acf722c
GuilhermeSantosCoelho <guilhermesantoscoelho2000@gmail.com>
2021-05-23 20:37:27 -0300

    Commit 9

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const MongoDB = require('mongodb');
 const router = express.Router()
-const uri = "mongodb+srv://new_pass:mongodbweb@cluster0.vhdw6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://guigui:trabweb@cluster0.vhdw6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoDB.MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 router.get('/', async (req, res) => {
